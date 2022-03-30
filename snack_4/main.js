611574a7a24f4afcc5e7c493e59d28a6e3da17ff
FedericoCapitani <federico.capitani@outlook.it>
2022-03-30 12:25:57 +0200

    snack 4

---
+++ snack_4/main.js
@@ -0,0 +1,20 @@
+/* In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa. */
+
+// array invitati
+const invitati = ["roberto", "giovanni", "maria", "luca", "sara"]
+const utente = prompt('Inserisci il tuo nome')
+
+let boolean = false;
+
+for (let i = 0; i < invitati.length; i++){
+    let controllo = invitati[i];
+    if (utente == controllo){
+        boolean = true;
+    }
+}
+
+if(boolean){
+    console.log(`Prego ${utente}, la festa inizierà a breve`);
+} else {
+    console.log('Spiacenti, il suo nome no risulta nella lista degli invitati');
+}
